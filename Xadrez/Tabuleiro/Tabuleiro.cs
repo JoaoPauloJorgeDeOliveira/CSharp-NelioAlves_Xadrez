2f3e6b96839251198f7e6482ff844ebdcb213f17
jDell <joaoolive@gmail.com>
2019-01-27 17:36:22 -0200

    Começando a implementar o tabuleiro

---
+++ Xadrez/Tabuleiro/Tabuleiro.cs
@@ -0,0 +1,16 @@
+﻿namespace tabuleiro
+{
+    class Tabuleiro
+    {
+        public int linhas { get; set; }
+        public int colunas { get; set; }
+        private Peca[,] pecas; // Privativa, pois só o Tabuleiro mexe nas peças.
+
+        public Tabuleiro(int linhas, int colunas)
+        {
+            this.linhas = linhas;
+            this.colunas = colunas;
+            this.pecas = new Peca[linhas,colunas];
+        }
+    }
+}
